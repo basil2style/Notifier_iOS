60bdf6e58d3b52c005903d48b7aedc81f06a7075
Basil <basiltalias92@gmail.com>
2017-04-11 19:11:21 -0400

    Added request for view controller

--- Notifier/ViewController.swift
+++ Notifier/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import UserNotifications
+
+//There are different kinds of notificaitons
 
 class ViewController: UIViewController {
+    
+    var isGrantedPermission = false
 
+    @IBAction func sendANotification(_ sender: UIButton) {
+        
+        if (isGrantedPermission) {
+            
+        }
+        
+    }
+    
+    @IBAction func viewPendingNotifications(_ sender: UIButton) {
+        
+    }
+    @IBAction func viewDeliveredNotifications(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) {
+            (isGranted, <#Error?#>) in
+            self.isGrantedPermission = isGranted
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
